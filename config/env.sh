0623b32e7204709121bdc75392fb9240e65da484
FuGuiLiu <liu997121@gmail.com>
2022-02-01 23:37:36 +0800

    update

---
+++ config/env.sh
@@ -0,0 +1,2 @@
+export JD_COOKIE="pt_key=AAJh3AKYADAEsTcgl7o-6nWhLKbhkHetKHB3NFX-uYDNSfe4nLOWBl7Bwtr3YpCAfuCioWimeo0;pt_pin=jd_610c8412c6366;&pt_key=AAJh3AL9ADAGMquOjwxZ1sZSerZBYthDMObM-czTkHhkHHHuxwjjvEIjEDut0Ztl5x7bAhCp1_4;pt_pin=jd_rHiByDWiFaBq;"
+export ZNS="true"
